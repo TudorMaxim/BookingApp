b9bc1de9f260c561925ec0ebf4132451acf6a8f9
Tudor Maxim <maximtudor98@gmail.com>
2021-04-06 13:55:27 +0300

    Added Navigation Dropdown and Password Reset Link

---
+++ booking-app-frontend/src/common/components/AppDropdown.tsx
@@ -0,0 +1,58 @@
+import { FunctionComponent, SetStateAction, Dispatch } from 'react';
+import { Dropdown } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import { useStore } from '../../context/store';
+import authService from '../../service/auth.service';
+
+interface AppDropdownProps {
+  name: string,
+  imageSrc: string,
+  show: boolean,
+  setShow: Dispatch<SetStateAction<boolean>>,
+}
+
+const AppDropdown: FunctionComponent<AppDropdownProps> = ({
+  name, imageSrc, show, setShow,
+}) => {
+  const { dispatch } = useStore();
+  return (
+    <div className="custom-dropdown">
+      <div
+        className="custom-dropdown-toggle"
+        aria-hidden="true"
+        onClick={() => setShow(!show)}
+        onKeyDown={() => setShow(!show)}
+      >
+        {name.split(' ')[0]}
+        <img className="user-profile-image-small" src={imageSrc} alt="Profile" />
+      </div>
+      <div className="custom-dropdown-menu" hidden={!show}>
+        <Link
+          className="nav-link"
+          to="/profile"
+          onClick={() => setShow(false)}
+        >
+          Profile
+        </Link>
+        <Link
+          className="nav-link"
+          to="/reset"
+          onClick={() => setShow(false)}
+        >
+          Reset Password
+        </Link>
+        <Dropdown.Divider />
+        <div
+          className="nav-link"
+          aria-hidden="true"
+          onClick={() => authService.logout(name, dispatch)}
+          onKeyDown={() => authService.logout(name, dispatch)}
+        >
+          Logout
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default AppDropdown;
